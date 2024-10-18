640a4e58458e9a26877245f29e82df19c8c2835a
22jz0107 <22jz0107@jec.aac.jp>
2024-10-18 12:23:34 +0900

    ko

---
+++ src/dao/DBManager.java
@@ -0,0 +1,28 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBManager {
+    private static final String DATABASE_NAME = "databaseg09";
+    private static final String USER = "new_root";
+    private static final String PASS = "#98MxdslOf;lg09";
+    private static final String PROPERTIES = "?characterEncoding=UTF-8&enabledTLSProtocols=TLSv1.2";
+    private static final String URL = "jdbc:mysql://158.101.151.242:3306/" + DATABASE_NAME + PROPERTIES;
+
+    // データベース接続を取得するメソッド
+    public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASS);
+    }
+
+    // データベースに接続するテストメソッド
+    public boolean testConnection() {
+        try (Connection conn = getConnection()) {
+            return conn != null;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
--- src/dao/UserDAO.java
+++ src/dao/UserDAO.java
@@ -3,6 +3,7 @@ package dao;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import model.User;
@@ -16,11 +17,10 @@ public class UserDAO {
 
     public boolean insertUser(User user) {
         String sql = "INSERT INTO user_table (id, us_name, us_password, us_tel_number, us_address, create_date, update_date) VALUES (?, ?, ?, ?, ?, ?, ?)";
-        // DBManagerのinstanceフィールドから直接Connectionを取得
-        try (Connection conn = dbManager.instance;
+        try (Connection conn = dbManager.getConnection();
              PreparedStatement pstmt = conn.prepareStatement(sql)) {
 
-            pstmt.setInt(1, user.getId().intValue()); // Long型のIDをint型に変換
+            pstmt.setInt(1, user.getId().intValue());
             pstmt.setString(2, user.getName());
             pstmt.setString(3, user.getPassword());
             pstmt.setLong(4, user.getTelNumber());
@@ -29,10 +29,50 @@ public class UserDAO {
             pstmt.setDate(7, new Date(user.getUpdateDate().getTime()));
 
             int rowsAffected = pstmt.executeUpdate();
-            return rowsAffected > 0; // 1行以上が挿入されたらtrueを返す
+            return rowsAffected > 0;
         } catch (SQLException e) {
             e.printStackTrace();
         }
         return false;
     }
+
+    public User getUserById(Long id) {
+        String sql = "SELECT us_name, us_password, us_tel_number, us_address, create_date, update_date FROM user_table WHERE id = ?";
+        try (Connection conn = dbManager.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setLong(1, id);
+            ResultSet rs = pstmt.executeQuery();
+
+            if (rs.next()) {
+                String name = rs.getString("us_name");
+                String password = rs.getString("us_password");
+                Long telNumber = rs.getLong("us_tel_number");
+                String address = rs.getString("us_address");
+                java.util.Date createDate = new java.util.Date(rs.getTimestamp("create_date").getTime());
+                java.util.Date updateDate = new java.util.Date(rs.getTimestamp("update_date").getTime());
+
+                return new User(id, name, password, telNumber, address, createDate, updateDate);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void printUser(User user) {
+        if (user != null) {
+            System.out.println("ユーザーID: " + user.getId());
+            System.out.println("ユーザー名: " + user.getName());
+            System.out.println("パスワード: " + user.getPassword());
+            System.out.println("電話番号: " + user.getTelNumber());
+            System.out.println("住所: " + user.getAddress());
+            System.out.println("作成日: " + user.getCreateDate());
+            System.out.println("更新日: " + user.getUpdateDate());
+        } else {
+            System.out.println("ユーザーが見つかりませんでした。");
+        }
+    }
 }
+
+// UserServiceとメインメソッドを別ファイルに作成
