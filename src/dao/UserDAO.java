ea504891220969bbf9a89428ad120be991e873e8
22jz0107 <22jz0107@jec.aac.jp>
2024-10-18 12:26:16 +0900

    aa

--- src/dao/UserDAO.java
+++ src/dao/UserDAO.java
@@ -1,36 +1,25 @@
 package dao;
 
 import java.sql.Connection;
-<<<<<<< HEAD
-=======
 import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
->>>>>>> branch 'main' of https://github.com/22jz0119/SoundDive.git
 
-import javax.naming.InitialContext;
-import javax.sql.DataSource;
+import model.User;
 
 public class UserDAO {
-    static DataSource ds;
+    private DBManager dbManager;
 
-    public Connection getConnection() throws Exception {
+    public UserDAO(DBManager dbManager) {
+        this.dbManager = dbManager;
+    }
 
-<<<<<<< HEAD
-        // 初期コンテキスト構築
-        InitialContext ic = new InitialContext();
-=======
     public boolean insertUser(User user) {
         String sql = "INSERT INTO user_table (id, us_name, us_password, us_tel_number, us_address, create_date, update_date) VALUES (?, ?, ?, ?, ?, ?, ?)";
         try (Connection conn = dbManager.getConnection();
              PreparedStatement pstmt = conn.prepareStatement(sql)) {
->>>>>>> branch 'main' of https://github.com/22jz0119/SoundDive.git
 
-<<<<<<< HEAD
-        // DBの接続先情報を取得（context.xmlの内容）
-        ds = (DataSource) ic.lookup("java:/comp/env/jdbc/book");
-=======
             pstmt.setInt(1, user.getId().intValue());
             pstmt.setString(2, user.getName());
             pstmt.setString(3, user.getPassword());
@@ -38,18 +27,13 @@ public class UserDAO {
             pstmt.setString(5, user.getAddress());
             pstmt.setDate(6, new Date(user.getCreateDate().getTime()));
             pstmt.setDate(7, new Date(user.getUpdateDate().getTime()));
->>>>>>> branch 'main' of https://github.com/22jz0119/SoundDive.git
 
-<<<<<<< HEAD
-        return ds.getConnection();
-=======
             int rowsAffected = pstmt.executeUpdate();
             return rowsAffected > 0;
         } catch (SQLException e) {
             e.printStackTrace();
         }
         return false;
->>>>>>> branch 'main' of https://github.com/22jz0119/SoundDive.git
     }
 
     public User getUserById(Long id) {
