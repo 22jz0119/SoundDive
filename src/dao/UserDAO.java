2eb2f2f55ca5b5bbebc2bebc33c2db6d16b2a047
22jz0126 <22jz0126@jec.ac.jp>
2024-10-18 12:24:07 +0900

    ｐｐ

--- src/dao/UserDAO.java
+++ src/dao/UserDAO.java
@@ -1,38 +1,21 @@
 package dao;
 
 import java.sql.Connection;
-import java.sql.Date;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
 
-import model.User;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
 
 public class UserDAO {
-    private DBManager dbManager;
+    static DataSource ds;
 
-    public UserDAO(DBManager dbManager) {
-        this.dbManager = dbManager;
-    }
+    public Connection getConnection() throws Exception {
 
-    public boolean insertUser(User user) {
-        String sql = "INSERT INTO user_table (id, us_name, us_password, us_tel_number, us_address, create_date, update_date) VALUES (?, ?, ?, ?, ?, ?, ?)";
-        // DBManagerのinstanceフィールドから直接Connectionを取得
-        try (Connection conn = dbManager.instance;
-             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+        // 初期コンテキスト構築
+        InitialContext ic = new InitialContext();
 
-            pstmt.setInt(1, user.getId().intValue()); // Long型のIDをint型に変換
-            pstmt.setString(2, user.getName());
-            pstmt.setString(3, user.getPassword());
-            pstmt.setLong(4, user.getTelNumber());
-            pstmt.setString(5, user.getAddress());
-            pstmt.setDate(6, new Date(user.getCreateDate().getTime()));
-            pstmt.setDate(7, new Date(user.getUpdateDate().getTime()));
+        // DBの接続先情報を取得（context.xmlの内容）
+        ds = (DataSource) ic.lookup("java:/comp/env/jdbc/book");
 
-            int rowsAffected = pstmt.executeUpdate();
-            return rowsAffected > 0; // 1行以上が挿入されたらtrueを返す
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return false;
+        return ds.getConnection();
     }
 }
