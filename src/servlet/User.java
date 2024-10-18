06ab1e92a6e6dc8b3fcaff15b11d1bc2295d4891
22jz0107 <22jz0107@jec.aac.jp>
2024-10-18 11:44:26 +0900

    aa

--- src/servlet/User.java
+++ src/servlet/User.java
@@ -4,9 +4,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,17 +12,21 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class User
+ * Servlet implementation class Oser
  */
 @WebServlet("/User")
 public class User extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-	
+    
 
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.setContentType("text/html");
+		response.setCharacterEncoding("Shift_JIS");
 		
 		PrintWriter out = response.getWriter();
 		
@@ -33,13 +35,15 @@ public class User extends HttpServlet {
         out.println("<body>");
         out.println("<h1>Hello, World!</h1>");
         out.println("<p>This is a simple servlet example.</p>");
-        String DATABASE_NAME = "information_schema";
-        String PROPERTIES = "?characterEncoding=UTF-8&serverTimezone=Asia/Tokyo";
-        String URL = "jdbc:mysql://localhost:3306/" + DATABASE_NAME + PROPERTIES;
+        String DATABASE_NAME = "databaseg09";
+        String PROPERTIES = "?characterEncoding=UTF-8&enabledTLSProtocols=TLSv1.2";
+//        String PROPERTIES = "?characterEncoding=UTF-8&serverTimezone=Asia/Tokyo&enabledTLSProtocols=TLSv1.2";
+        String URL = "jdbc:mysql://158.101.151.242:3306/" + DATABASE_NAME + PROPERTIES;
+//        String URL = "jdbc:mysql://158.101.151.242:3306/databaseg09?characterEncoding=UTF-8&enabledTLSProtocols=TLSv1.2";
         //DB接続用・ユーザ定数
-        String USER = "root";
-        String PASS = "";
-
+        String USER = "new_root";
+        String PASS = "#98MxdslOf;lg09";
+        out.println(URL);
         try {
             //MySQL に接続する
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -52,21 +56,21 @@ public class User extends HttpServlet {
             // 接続を閉じる
             conn.close();
 
-        } catch (SQLException | ClassNotFoundException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
         out.println("</body>");
         out.println("</html>");
 		
-		RequestDispatcher dispatcher = request.getRequestDispatcher("userjsp.jsp");
-        dispatcher.forward(request, response);
+		
 	}
 
-	
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
-	
 }
